f321b4b4a8b7b442753b1c56f60dd0531115ce1b
IvanSakaev <x-sakaev@yandex.ru>
2025-02-09 16:47:59 +0300

    Add labirinth rendering

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+let width = 8;
+let height = 6;
+let walls = [
+  [0, 0, 0, 6],
+  [0, 0, 8, 0],
+  [0, 6, 8, 6],
+  [8, 0, 8, 6],
+  [0, 4, 1, 4],
+  [1, 1, 1, 3],
+  [1, 4, 1, 5],
+  [1, 3, 5, 3],
+  [2, 1, 2, 2],
+  [2, 4, 2, 5],
+  [2, 5, 5, 5],
+  [3, 1, 3, 2],
+  [3, 3, 3, 4],
+  [3, 1, 5, 1],
+  [4, 4, 5, 4],
+  [4, 2, 6, 2],
+  [5, 0, 5, 1],
+  [5, 3, 5, 4],
+  [5, 4, 6, 4],
+  [6, 1, 6, 4],
+  [6, 5, 6, 6],
+  [6, 1, 7, 1],
+  [6, 3, 7, 3],
+  [6, 5, 7, 5],
+  [7, 4, 7, 5],
+  [7, 2, 8, 2],
+];
+
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
@@ -20,16 +51,23 @@ let speedy = 0;
 let planets = [];
 
 if (mode === 'labirinth') {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < walls.length; i++) {
     let elem = document.createElement('div');
-    elem.classList.add('vertical');
-    elem.style.left = i*10 + '%';
+    if (walls[i][0] === walls[i][2]) {
+      elem.classList.add('vertical');
+      elem.style.height = (walls[i][3] - walls[i][1]) * 10 + '%';
+    } else if (walls[i][1] === walls[i][3]) {
+      elem.classList.add('horizontal');
+      elem.style.width = (walls[i][2] - walls[i][0]) * 10 + '%';
+    }
+    elem.style.left = walls[i][0]*10 + '%';
+    elem.style.top = walls[i][1]*10 + '%';
     labirinth.appendChild(elem);
   }
 }
 
 if (mode === 'random') {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 25; i++) {
     let elem = document.createElement('img');
     elem.classList.add('planet');
     elem.src = 'assets/meteor.png';
@@ -59,10 +97,10 @@ document.addEventListener('click', (e) => {
 })
 
 function moveByGravity(planet) {
-  let dist = Math.sqrt((planet[0] - x) * (planet[0] - x) + (planet[1] - y) * (planet[1] - y));
+  let dist = Math.sqrt((planet[0] - x) * (planet[0] - x) + (planet[1] - y) * (planet[1] - y)) / 5;
   if (dist > 1) {
-    speedx += 5 * (planet[0] - x) / dist / dist;
-    speedy += 5 * (planet[1] - y) / dist / dist;
+    speedx += (planet[0] - x) / dist / dist;
+    speedy += (planet[1] - y) / dist / dist;
   }
 }
 
