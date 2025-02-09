13536ca4502b5910cac3716997acd0b483f889af
IvanSakaev <x-sakaev@yandex.ru>
2025-02-09 11:06:04 +0300

    Add planet gravity

--- index.js
+++ index.js
@@ -13,15 +13,7 @@ let x = 100;
 let y = 100;
 let speedx = 0;
 let speedy = 0;
-let mousex = 0;
-let mousey = 0;
-let mousemove = false;
-
-document.addEventListener('mousemove', (e) => {
-  mousex = e.clientX - 8;
-  mousey = e.clientY - 8;
-  mousemove = true;
-})
+let planets = [];
 
 document.addEventListener('click', (e) => {
   mousex = e.clientX - 8;
@@ -34,38 +26,41 @@ document.addEventListener('click', (e) => {
       elem.style.left = mousex - 15 + 'px';
       elem.style.top = mousey - 16 + 'px';
       labirinth.appendChild(elem);
+      planets.push([mousex - 15, mousey - 16]);
     }
   }
 })
 
-setInterval(() => {
-  if (mousemove) {
-    let dist = Math.sqrt((mousex - x) * (mousex - x) + (mousey - y) * (mousey - y));
-    if (dist > 1) {
-      speedx += 10 * (mousex - x) / dist / dist;
-      speedy += 10 * (mousey - y) / dist / dist;
-    }
-    x += speedx;
-    y += speedy;
-
-    if (x < (hero.width / 2)) {
-      x = hero.width / 2;
-      speedx = 0;
-    } else if (x > (800 - hero.width / 2)) {
-      x = (800 - hero.width / 2);
-      speedx = 0;
-    }
-    if (y < (hero.height / 2)) {
-      y = hero.height / 2;
-      speedy = 0;
-    } else if (y > (800 - hero.height / 2)) {
-      y = (800 - hero.height / 2);
-      speedy = 0;
-    }
-
-    hero.style.left = x - (hero.width / 2) + 'px';
-    hero.style.top = y - (hero.height / 2) + 'px';
-    // hero.style.left = mousex - (hero.width / 2) + 'px';
-    // hero.style.top = mousey - (hero.height / 2) + 'px';
+function moveByGravity(planet) {
+  let dist = Math.sqrt((planet[0] - x) * (planet[0] - x) + (planet[1] - y) * (planet[1] - y));
+  if (dist > 1) {
+    speedx += 5 * (planet[0] - x) / dist / dist;
+    speedy += 5 * (planet[1] - y) / dist / dist;
   }
+}
+
+setInterval(() => {
+  for (let i = 0; i < planets.length; i++){
+    moveByGravity(planets[i]);
+  }
+  x += speedx;
+  y += speedy;
+
+  if (x < (hero.width / 2)) {
+    x = hero.width / 2;
+    speedx = 0;
+  } else if (x > (800 - hero.width / 2)) {
+    x = (800 - hero.width / 2);
+    speedx = 0;
+  }
+  if (y < (hero.height / 2)) {
+    y = hero.height / 2;
+    speedy = 0;
+  } else if (y > (800 - hero.height / 2)) {
+    y = (800 - hero.height / 2);
+    speedy = 0;
+  }
+
+  hero.style.left = x - (hero.width / 2) + 'px';
+  hero.style.top = y - (hero.height / 2) + 'px';
 }, 1000 / 60);
